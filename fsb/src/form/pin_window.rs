39066e3e3b6d85e286e50fa4e9799197bc7b449c
壹零 <guchengxi1994@qq.com>
2023-12-08 17:45:55 +0800

    update window

--- fsb/src/form/pin_window.rs
+++ fsb/src/form/pin_window.rs
@@ -1,4 +1,33 @@
 slint::slint! {
+import { VerticalBox , HorizontalBox, Button,ListView,CheckBox} from "std-widgets.slint";
+
+export component CustomListView inherits Window {
+    ListView {
+        width: 300px;
+        height: 500px;
+        for data in [
+            { text: "Blue", color: #0000ff, bg: #eeeeee},
+            { text: "Red", color: #ff0000, bg: #eeeeee},
+            { text: "Green", color: #00ff00, bg: #eeeeee},
+            { text: "Yellow", color: #ffff00, bg: #222222 },
+            { text: "Black", color: #000000, bg: #eeeeee },
+            { text: "White", color: #ffffff, bg: #222222 },
+            { text: "Magenta", color: #ff00ff, bg: #eeeeee },
+            { text: "Cyan", color: #00ffff, bg: #222222 },
+        ] : Rectangle {
+            height: 30px;
+            // background: data.bg;
+            width: parent.width;
+            CheckBox {}
+            Text {
+                x: 0;
+                text: data.text;
+                // color: data.color;
+            }
+        }
+    }
+}
+
 export component PinWindow inherits Window {
         property <bool> active: false;
         no-frame: true;
@@ -7,6 +36,7 @@ export component PinWindow inherits Window {
         property <length> dynamic_width: 100px;
         property <length> dynamic_height: 100px;
         property <length> radius: 100px;
+        in property <string> title_name : "Default";
 
 
         callback mouse_move(length, length);
@@ -33,11 +63,74 @@ export component PinWindow inherits Window {
 
         TouchArea {
             moved => {
-                root.mouse_move(self.mouse-x - self.pressed-x, self.mouse-y - self.pressed-y);
+                if (!root.active){
+                    root.mouse_move(self.mouse-x - self.pressed-x, self.mouse-y - self.pressed-y);
+                }
             }
             clicked => {
-                root.active = !root.active;
+                if (!root.active){
+                    root.active = !root.active;
+                }
+                // root.active = !root.active;
             }
+
+            VerticalLayout {
+                width: dynamic_width;
+                height: dynamic_height;
+                visible: root.active;
+                alignment: center;
+
+
+                Rectangle {
+                    height: 30px;
+                    TouchArea {
+                        moved => {
+                            if (root.active){
+                                root.mouse_move(self.mouse-x - self.pressed-x, self.mouse-y - self.pressed-y);
+                            }
+                        }
+                        HorizontalLayout {
+                        alignment: space-around;
+                        Image {
+                            source: @image-url("icon.png") ;
+                            width: 30px;
+                            height: 30px;
+                        }
+                        Rectangle {
+                            width: dynamic_width -70px ;
+                            height: 30px;
+                            Text {
+                                text: title_name;
+                            }
+                        }
+                        TouchArea {
+                            width: 30px;
+                            height: 30px;
+                            clicked => {
+                                root.active = !root.active;
+                            }
+                            Image {source: @image-url("hide.png") ;
+                            width: 20px;
+                            height: 20px;}
+                        }
+                    }
+                    }   
+                }
+                Rectangle {
+                    height: dynamic_height - 30px;
+                    width: dynamic_width;
+                    VerticalLayout {
+                        alignment: start;
+                        HorizontalLayout {
+                            alignment: center;
+                            CustomListView{}
+                        }
+                        
+                    }
+                    
+                }
+            }
+
         }
     }
 
